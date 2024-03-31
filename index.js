5a2364593075b04717502dd28b4024c867ab351a
aryanRP7 <aryanalls100@gmail.com>
2024-03-31 18:35:21 +0530

    1st  commit

---
+++ index.js
@@ -0,0 +1,68 @@
+alert("Hello User!");
+
+
+//detecting button press/click on browser
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
+{
+document.querySelectorAll(".drum")[i].addEventListener("click",function ()
+    {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    animation(buttonInnerHTML);
+    
+    });
+}
+
+//Detecting Keyboard key press
+//keypress detecting for entire browser hence eventListener assigned to entire document.
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    animation(event.key);
+})
+
+
+
+function makeSound(key){
+
+    switch(key)
+        {
+        case "w": 
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+        case "a": 
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+        case "s": 
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+        case "d": 
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+        case "j": 
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+        case "k": 
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+        case "l": 
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+        default :
+        console.log(buttonInnerHTML);
+
+        }
+}
+
+function animation(currentKey){
+    var activebuttoon = document.querySelector("."+currentKey);
+    activebuttoon.classList.add("pressed");
+    setTimeout(function(){
+        activebuttoon.classList.remove("pressed")},100);
+    }
